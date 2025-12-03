e896202e74cee9f28fbc15e6e4c041ffd11d2cef
Arslan Aslam <arslanaslam7282@gmail.com>
2025-12-03 20:57:09 +0500

    implicity or explicity

--- Config.py
+++ Config.py
@@ -3,6 +3,9 @@ from appium import webdriver
 from typing import Any, Dict
 from appium.options.common import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
+import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 cap: Dict[str, Any] = {
     'platformName': 'Android',
@@ -20,20 +23,26 @@ cap: Dict[str, Any] = {
 url = 'http://127.0.0.1:4723'
 
 driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(cap))
+driver.implicitly_wait(10)
 
-VALID_CREDENTIALS = {
-    'Email': 'arslanaslam@gmail.com',
-    'Password': '123456789',
-}
+driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='Phone, 2 notifications').click()
 
-el = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='Contacts, 0 notifications')
+wait = WebDriverWait(driver, 10)
+el = wait.until(EC.presence_of_element_located(AppiumBy.ACCESSIBILITY_ID, "com.android.dialer:id/contactsButton"))
 el.click()
 
-driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@text="Search among 602 contact(s)"]').send_keys("Ahmad")
+driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@content-desc="New Contact"]').click()
+time.sleep(5)
 
-def test_001_singin(self):
-    driver.find_element(by=AppiumBy.XPATH, value="//android.widget.EditText[contains(@hint,'Email')]").send_keys(VALID_CREDENTIALS['Email'])
+driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@text="Name"]').send_keys("test Ali")
 
-# driver.quit()
+driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@text="Phone"]').send_keys("123456789")
+
+driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@text="Done"]').click()
+driver.close()
+
+# driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@text="Search among 602 contact(s)"]').send_keys("Ahmad")
+
+driver.quit()
 
 
